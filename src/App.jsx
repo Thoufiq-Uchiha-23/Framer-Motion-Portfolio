de7c02deb1e65d3799e12d92ed96a73bc746197c
Thoufiq-Izhar-23 <thoufiqizhar022@gmail.com>
2024-09-04 19:21:32 +0530

    app jsx updated

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-rose-900 flex justify-center items-center text-2xl'><h1>App</h1></div>
+  )
+}
+
+export default App
