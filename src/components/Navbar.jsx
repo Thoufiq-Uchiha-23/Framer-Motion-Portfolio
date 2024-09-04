af1f09e6fcc9dcadc8faa489fb6a9126cff89f86
Thoufiq-Izhar-23 <thoufiqizhar022@gmail.com>
2024-09-04 22:12:26 +0530

    Mission: Navbar Section Completed

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import logo from "../assets/images/kevinRushLogo.png";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaSquareTwitter } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa";
+
+const Navbar = () => {
+  return (
+    <nav className="mb-20 w-full flex justify-between items-center py-6 px-20">
+        <div className="flex  items-center">
+            <img className="mx-2 w-10" src={logo} alt="" />
+        </div>
+        <div className="flex m-8 items-center justify-center gap-4 text-2xl">
+          <FaLinkedin />
+          <FaGithub />
+          <FaInstagram />
+          <FaSquareTwitter />
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
