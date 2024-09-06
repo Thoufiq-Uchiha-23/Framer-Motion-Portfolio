dcecc6319fa588674ffe548f8af56317d7c2396d
Thoufiq-Izhar-23 <thoufiqizhar022@gmail.com>
2024-09-06 22:28:01 +0530

    Mission: Navbar section completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,18 +6,18 @@ import { FaInstagram } from "react-icons/fa";
 
 const Navbar = () => {
   return (
-    <nav className="mb-20 w-full flex justify-between items-center py-6 px-20">
-        <div className="flex  items-center">
-            <img className="mx-2 w-10" src={logo} alt="" />
-        </div>
-        <div className="flex m-8 items-center justify-center gap-4 text-2xl">
-          <FaLinkedin />
-          <FaGithub />
-          <FaInstagram />
-          <FaSquareTwitter />
-        </div>
+    <nav className="mb-20 w-full flex justify-between items-center py-6  px-10 lg:px-20">
+      <div className="flex  items-center">
+        <img className="mx-2" width={50} src={logo} alt="Kevin Rush" />
+      </div>
+      <div className="flex m-8 items-center justify-center gap-4 text-2xl">
+        <FaLinkedin />
+        <FaGithub />
+        <FaInstagram />
+        <FaSquareTwitter />
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
