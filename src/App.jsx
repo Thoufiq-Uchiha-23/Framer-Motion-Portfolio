b6106217433fbd30395ceb04175c292c2e01b2b9
Thoufiq-Izhar-23 <thoufiqizhar022@gmail.com>
2024-09-11 18:31:06 +0530

    minor change

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
@@ -13,7 +12,7 @@ const App = () => {
   return (
     <div className="flex justify-center items-center overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection-text:text-cyan-900">
       <div className="fixed top-0 -z-10 h-full w-full">
-        <div class="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+        <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
       </div>
       <div className="container mx-auto px-5">
         <Navbar />
